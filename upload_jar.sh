696b03c6a605f195762fc8555495013937c005fc
Gijs Peters <petersg@Gijss-MBP.ttg.global>
2018-07-10 09:05:11 +0200

    1531206311

--- upload_jar.sh
+++ upload_jar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd ~/PubInt_jar
-cp '/home/gijs/IdeaProjects/PubInt/target/PubInt-0.1-SNAPSHOT.jar' 'PubInt-0.1-SNAPSHOT.jar'
+cp '~/IdeaProjects/PubInt/target/PubInt-0.1-SNAPSHOT.jar' 'PubInt-0.1-SNAPSHOT.jar'
 git stage *
 git commit -m $(date +%s)
 git push
